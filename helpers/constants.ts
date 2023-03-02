fa05c43c3c3d75651e345ffbe5a2183a8313e326
Mariia Synelnyk <44849051+Mariesnlk@users.noreply.github.com>
2023-03-02 17:40:41 +0100

    Create constants.ts

---
+++ helpers/constants.ts
@@ -0,0 +1,10 @@
+export const StakingTypes = {
+    CORE_BASED: 'Core Base Staking',
+    STAKING: 'Staking',
+    SANCTIONS: 'Sanctions Staking',
+    COOLDOWN_STAKING: 'Cooldown Staking'
+};
+export const MIN_STAKING_PERIOD = 60 * 60 * 24 * 365;
+export const EARLY_FEE = 1000; // 10 %
+export const SANCTIONS_FEE = 2000; // 10 %
+export const APY = 1000; // 10 %
